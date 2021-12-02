ac7b8bce0b223b57e777669e4a841163727cafa1
brianminges <btminges@gmail.com>
2021-12-01 19:12:56 -0600

    initial commit

---
+++ scripts.js/database.js
@@ -0,0 +1,15 @@
+/*
+
+    This module contains all of the data, or state, for the
+    application. 
+
+*/
+const database = {
+    cones: [
+        { id: 1, type: "kiddie", price: 1 },
+        { id: 2, type: "cake", price: 2 },
+        { id: 3, type: "waffle", price: 3 },
+        { id: 4, type: "chocolate-dipped waffle", price: 5 } 
+    ]
+
+}
