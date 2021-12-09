476a0845184d644f9a173e33c1364ce6790b2143
Kyle Buckner <kylebuckner88@gmail.com>
2021-12-08 18:47:24 -0600

    kylescommit

--- scripts.js/database.js
+++ scripts.js/database.js
@@ -13,3 +13,20 @@ const database = {
     ]
 
 }
+
+    toppings: [
+        { id: 1, name: "sprinkles", price: .50},
+        { id: 2, name: "nuts", price: .75},
+        { id: 3, name: "shell", price: .50},
+        { id: 4, name: "candy", price: .50}
+    ]
+
+
+
+    export const getToppings = () => {
+    return database.toppings.map(topping => ({...topping}))
+    }
+
+    export const setToppings = (id) => {
+    database.orderBuilder.toppingId = id
+    }
